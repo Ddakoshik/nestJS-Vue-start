20b0d87b19ac9013c6ca68fed5c40eca012faecf
Andrii Ishchenko <aishchenko@deloitte.com>
2020-11-14 15:03:08 +0200

    add .env file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap')
 }
 bootstrap();
